94ef5cd516a356356bd4addeb15c63b6147ea84c
Sungmin <ijbmsm@naver.com>
2022-02-21 10:26:56 +0900

    init

---
+++ src/main/java/com/sungmin/webex/WebexApplication.java
@@ -0,0 +1,13 @@
+package com.sungmin.webex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sungmin/webex/WebexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungmin.webex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
